ba08ed860e3f43ebad8a15a2ce2d538fe49f72b6
colinmiller <colinchmiller@gmail.com>
2016-04-03 09:19:56 -0500

    first pass at basic logic structure, failing on print syntax to see values

--- sumPrimes.py
+++ sumPrimes.py
@@ -1,25 +1,17 @@
 primeNumbers = [2];
 primeSum = 2;
 
-def primeCalc():
+def primeCalc(primeSum, primeNumbers):
     for x in range(3,1000):
-        if primeCheck(x) == False;
+        if primeCheck(x) != False:
+            primeNumbers.append(x);
+            primeSum = primeSum + x;
+            print "Prime Sum is:" primeSum;
+            print "Prime Numbers:" primeNumbers
 
 def primeCheck(x):
     for y in primeNumbers:
         if x % y == 0:
-            return False
+            return False;
 
-newbie();
-
-for x in range(3,1000):
-    for y in primeNumbers:
-        if x % y == 0:
-            primeTest = false;
-            return
-        else:
-            primeTest = true;
-    if primeTest == true:
-        primeNumbers.push(x);
-        primeSum = primeSum + x;
-        print(primeSum);
+primeCalc(primeSum, primeNumbers);
